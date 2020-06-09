e0e309ebab8a3ef17b8f000310fe989cb7994d43
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-06-09 21:29:22 +0900

    add always

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,10 +54,12 @@ $(function(){
       $('.MessageField').append(html);
       $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
       $('form')[0].reset();
-      $('.SubmitBtn').prop('disabled', false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+    })
+    .always(function() {
+      $('.SubmitBtn').prop('disabled', false);
     });
   })
 });
