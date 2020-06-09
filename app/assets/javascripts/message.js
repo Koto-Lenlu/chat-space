a597c7cf33f18f7a5efecf47a14f45bc398d0589
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-06-09 20:50:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,41 +1,39 @@
 $(function(){
   function buildHTML(message){
     if ( message.image ) {
-      let html =
-        `<div class="MessageBox">
-          <div class="MessageInfo">
-            <div class="MessageInfo__userName">
-              ${message.user_name}
-            </div>
-            <div class="MessageInfo__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="Message">
-            <p class="Message__content">
-              ${message.content}
-            </p>
-            <img class="Message__image" src="${message.image}">
-          </div>
-        </div>`
+      let html = `<div class="MessageBox">
+                    <div class="MessageInfo">
+                      <div class="MessageInfo__userName">
+                        ${message.user_name}
+                      </div>
+                      <div class="MessageInfo__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="Message">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                      <img class="Message__image" src="${message.image}">
+                    </div>
+                  </div>`
       return html;
     } else {
-      let html =
-      `<div class="MessageBox">
-        <div class="MessageInfo">
-          <div class="MessageInfo__userName">
-            ${message.user_name}
-          </div>
-          <div class="MessageInfo__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="Message">
-          <p class="Message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
+      let html = `<div class="MessageBox">
+                    <div class="MessageInfo">
+                      <div class="MessageInfo__userName">
+                        ${message.user_name}
+                      </div>
+                      <div class="MessageInfo__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="Message">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
       return html;
     };
   }
